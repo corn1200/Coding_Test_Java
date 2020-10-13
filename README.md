e325edd480d747a824f940b478d23d4eab44c95b
corn1200 <corn120033@gmail.com>
2020-10-13 17:54:40 +0900

    MeetingRoom Code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-"# Coding_Test_Java" 
+# MeetingRoom
+
+## Problem
+
+input: [[0,30],[5,10],[15,20]]
+
+Output: false
+* * *
+input: [[7,10],[2,4]]
+
+Output: true
---
+++ src/MeetingRoom.java
@@ -0,0 +1,60 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Interval{
+    int start;
+    int end;
+    Interval() {
+        this.start = 0;
+        this.end = 0;
+    }
+
+    Interval(int s, int e) {
+        this.start = s;
+        this.end = e;
+    }
+}
+public class MeetingRoom {
+    public static void main(String[] args) {
+        MeetingRoom a = new MeetingRoom();
+
+        Interval int1 = new Interval(15, 20);
+        Interval int2 = new Interval(5, 10);
+        Interval int3 = new Interval(0, 30);
+
+        Interval[] intervals = {int1, int2, int3};
+        System.out.println(a.solve(intervals));
+    }
+
+    public boolean solve(Interval[] intervals) {
+        if (intervals == null) return false;
+        // 1 sorting
+        print(intervals);
+        Arrays.sort(intervals, Comp);
+        System.out.println("===after sort===");
+        print(intervals);
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i - 1].end > intervals[i].start) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void print(Interval[] intervals) {
+        for (int i = 0; i < intervals.length; i++) {
+            Interval in = intervals[i];
+            System.out.println(in.start + " " + in.end);
+        }
+    }
+    Comparator<Interval> Comp = new Comparator<Interval>() {
+
+        @Override
+        public int compare(Interval a, Interval b) {
+            return a.start - b.start;
+        }
+    };
+}
