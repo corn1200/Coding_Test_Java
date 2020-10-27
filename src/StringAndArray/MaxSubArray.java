6e6dce481220bef4d8a8092854bf8bed98d5d5f7
corn1200 <corn120033@gmail.com>
2020-10-27 17:36:03 +0900

    MaxSubArray

---
+++ src/StringAndArray/MaxSubArray.java
@@ -0,0 +1,19 @@
+package StringAndArray;
+
+public class MaxSubArray {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(nums));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int newSum = nums[0];
+        int max = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            newSum = Math.max(nums[i], newSum + nums[i]);
+            max = Math.max(newSum, max);
+        }
+        return max;
+    }
+}
