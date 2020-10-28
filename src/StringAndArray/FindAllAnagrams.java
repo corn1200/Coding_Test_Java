8efdb82aece2f52b445187169ef8f6c210cc5c66
corn1200 <corn120033@gmail.com>
2020-10-28 15:29:48 +0900

    FindAllAnagrams

---
+++ src/StringAndArray/FindAllAnagrams.java
@@ -0,0 +1,45 @@
+package StringAndArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllAnagrams {
+    public static void main(String[] args) {
+        FindAllAnagrams a = new FindAllAnagrams();
+        String txt = "BACDGABCDA";
+        String pat = "ABCD";
+        System.out.println(a.findAnagrams(txt, pat));
+    }
+
+    public List<Integer> findAnagrams(String txt, String pat) {
+        List<Integer> result = new ArrayList<>();
+        if (txt == null || txt.length() == 0 || pat == null || pat.length() == 0 || txt.length() < pat.length()) {
+            return result;
+        }
+        int[] patArr = new int[256];
+        for (int i = 0; i < pat.length(); i++) {
+            patArr[pat.charAt(i)]++;
+        }
+
+        for (int i = 0; i < txt.length() - pat.length() + 1; i++) {
+            int[] txtArr = new int[256];
+            for (int j = 0; j < pat.length(); j++) {
+                txtArr[txt.charAt(i+j)]++;
+            }
+
+            if (check(patArr, txtArr)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    private boolean check(int[] patArr, int[] txtArr) {
+        for (int i = 0; i < patArr.length; i++) {
+            if (patArr[i] != txtArr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
