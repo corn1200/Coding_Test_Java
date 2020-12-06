96e244f9cbc3f29e56485b6ba269d9fad54e43ae
corn1200 <corn120033@gmail.com>
2020-12-06 17:02:33 +0900

    feat: RemoveInvalidParentheses 주석을 추가하였습니다.

--- src/Graph_DfsAndBfs/RemoveInvalidParentheses.java
+++ src/Graph_DfsAndBfs/RemoveInvalidParentheses.java
@@ -2,6 +2,10 @@ package Graph_DfsAndBfs;
 
 import java.util.*;
 
+// 일련의 문장이 프로그래밍 문법을 지키고 있는지 확인하고
+// 문법을 지키지 않는 문장을 수정하여 유효한 프로그래밍
+// 문장으로 변환 후 저장합니다. 모든 변환 경우를 저장 후
+// 저장한 값을 출력합니다.
 public class RemoveInvalidParentheses {
     public static void main(String[] args) {
         String string = "(a)())()";
@@ -9,6 +13,14 @@ public class RemoveInvalidParentheses {
         System.out.println(object.removeInvalidParentheses(string));
     }
 
+    // 매개변수로 유효한 문법인지 확인할 문장을 받습니다.
+    // 문장이 유효할 경우 반환할 List 구조에 저장합니다.
+    // 유효하지 않을 경우 Queue 구조에 추가하고 Set 구조에
+    // 추가하여 괄호를 하나씩 제거하면서 유효한 문장을 만듭니다.
+    // 괄호를 하나씩 제거하며 수정된 문장을 Queue 에 추가하며
+    // 다시 유효한 문법인지를 확인하고 유효할시 List 에 저장합니다.
+    // 유효와 별개로 중복으로 수정된 문장인가를 판별하는 Set 에는
+    // 매번 저장합니다.
     private List<String> removeInvalidParentheses(String string) {
         List<String> result = new ArrayList<>();
         if (string == null) {
@@ -47,6 +59,10 @@ public class RemoveInvalidParentheses {
         return result;
     }
 
+    // 문장이 프로그래밍 문법을 지키는지 확인합니다.
+    // 좌측 괄호보다 우측 괄호가 먼저 나오거나
+    // 괄호의 쌍이 맞지 않으면 유효하지 않음을
+    // 반환합니다.
     private boolean isValid(String string) {
         int count = 0;
         for (char ch : string.toCharArray()) {
