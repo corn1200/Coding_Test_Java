5d778f4ccb3836466fd02ff294ff20778956a150
corn1200 <corn120033@gmail.com>
2020-12-13 18:52:09 +0900

    refactor: CourseSchedule 코드를 정리했습니다.

--- src/Graph_DfsAndBfs/CourseSchedule.java
+++ src/Graph_DfsAndBfs/CourseSchedule.java
@@ -2,6 +2,8 @@ package Graph_DfsAndBfs;
 
 import java.util.*;
 
+// nums 배열이 순서 규칙을 잘 지킨채로
+// 정렬되어 있는가를 판단하는 프로그램입니다.
 public class CourseSchedule {
 
     public static void main(String[] args) {
@@ -10,13 +12,11 @@ public class CourseSchedule {
                 { 2, 1 },
                 { 3, 2 } };
 
-        int[][] nums2 = {{ 1, 0 },
-                { 0, 1 } };
-
         CourseSchedule a = new CourseSchedule();
         System.out.println(a.solve(course, nums));
     }
 
+    //
     public boolean solve(int courseNumber, int[][] nums) {
         if (courseNumber <= 0)
             return false;
@@ -25,6 +25,8 @@ public class CourseSchedule {
         int[] inDegree = new int[courseNumber];
 
         int numsLength = nums.length;
+
+        //
         for (int i = 0; i < numsLength; i++) {
             inDegree[nums[i][1]]++;
         }
@@ -38,12 +40,11 @@ public class CourseSchedule {
         while (!queue.isEmpty()) {
             int zeroVal = queue.poll();
             for (int i = 0; i < numsLength; i++) {
-                System.out.println("=============");
                 if (zeroVal == nums[i][0]) {
-                    System.out.println("zeroVal "+zeroVal+" i "+i);
                     inDegree[nums[i][1]]--;
-                    if (inDegree[nums[i][1]] == 0)
+                    if (inDegree[nums[i][1]] == 0) {
                         queue.offer(nums[i][1]);
+                    }
                 }
             }
         }
